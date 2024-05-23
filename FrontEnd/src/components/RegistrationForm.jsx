8fd33f86585e02face7acc7178188eeac0004e3b
panciut <pancimarco02@gmail.com>
2024-05-23 17:07:46 +0200

    page position fix

--- FrontEnd/src/components/RegistrationForm.jsx
+++ FrontEnd/src/components/RegistrationForm.jsx
@@ -1,6 +1,5 @@
-
-import UniDomusLogo from "/UniDomusLogo.png"
-import { useState} from "react"
+import UniDomusLogo from "/UniDomusLogo.png";
+import { useState } from "react";
 import PopoverInfo from "./PopoverInfo.jsx";
 import { useNavigate } from "react-router-dom";
 import { API_BASE_URL } from "../constant";
@@ -10,210 +9,203 @@ import { useAuth } from "../AuthContext";
 export default function RegistrationForm() {
   const { login } = useAuth();
   const navigate = useNavigate();
-    const [formData, setFormData]= useState({
-      "username":"",
-      "email":"",
-      "password":""
-    })
-    const [formDataErr, setFormDataErr]= useState({
-      "usernameErr":"",
-      "emailErr":"",
-      "passwordErr":""
-    })
-    function handleChangeInput(e){
-      const { name, value } = e.target;
-      setFormData({
-        ...formData,
-        [name]: value
-      }); 
-    }
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [formDataErr, setFormDataErr] = useState({
+    usernameErr: "",
+    emailErr: "",
+    passwordErr: "",
+  });
 
-    function handleSubmit(e){
-      e.preventDefault();
-      
-      
-      const bodyForm={
-        username: formData.username,
-        email: formData.email,
-        password: formData.password
-      }
-      fetch(`${API_BASE_URL}users/registration`,{
-          method: 'POST',
-          headers:{
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(bodyForm)
-      })
-      .then((res)=>{
-         
-        
-        if(res.ok){
-          //console.log("ciao")
-          navigate('/');
-        }
-        else{
-          if(res.status=="400"){
-            setFormData({username: "", email: "", password :""})
-            res.json().then((json)=>{const errors=json.errors;
-              errors.map((element)=>{
-                setFormDataErr({
-                  ...formDataErr,
-                  [`${element.field}Err`]: element.message
-                })
-              })})
-              
-              
-          }
-          else if(res.status=="500"){
-            res.json().then((json)=>{alert(`${json.message}: ${json.reason}`)})
-            
-          }
-        }
-          
-      }
-      ).catch((e)=>console.log(e))
-    }
-    async function handleGoogleLogin(credentialResponse) {
-      try {
-        console.log(credentialResponse);
-        const id_token  = credentialResponse.credential;
-        console.log(id_token);
-        const bodyForm = {
-          token: id_token,
-        };
-  
-        const response = await fetch(`${API_BASE_URL}users/auth/google`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(bodyForm),
-        });
-  
-        if (response.ok) {
-          const data = await response.json();
-          // Store JWT in local storage
-          login(data.token); // Assume server sends token as { token: 'jwt_token' }
+  function handleChangeInput(e) {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const bodyForm = {
+      username: formData.username,
+      email: formData.email,
+      password: formData.password,
+    };
+    fetch(`${API_BASE_URL}users/registration`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyForm),
+    })
+      .then((res) => {
+        if (res.ok) {
           navigate("/");
         } else {
-          throw new Error("Google authentication failed");
+          if (res.status === 400) {
+            setFormData({ username: "", email: "", password: "" });
+            res.json().then((json) => {
+              const errors = json.errors;
+              errors.map((element) => {
+                setFormDataErr({
+                  ...formDataErr,
+                  [`${element.field}Err`]: element.message,
+                });
+              });
+            });
+          } else if (res.status === 500) {
+            res.json().then((json) => {
+              alert(`${json.message}: ${json.reason}`);
+            });
+          }
         }
-      } catch (error) {
-        console.error("Error:", error.message);
-        alert(error.message);
-      }
-    }
-
-    return (
-      <>
-        
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-28 w-auto"
-              src={UniDomusLogo}
-              alt="Unidomus"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign up to a new account
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={handleSubmit} method="POST">
-            <div>
-                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                  Username
-                </label>
-                
-                <div className="mt-2">
-                  <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    autoComplete="username"
-                    required
-                    value={formData.username}
-                    onChange={handleChangeInput}
-                    className="block w-full rounded-md border-0 py-1.5 text-center text-gray-950 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-700 sm:text-sm sm:leading-6"
-                  />
-                  {formDataErr.usernameErr && <p className="text-red-600 text-xs mt-1">{formDataErr.usernameErr}</p>}
-                </div>
-              </div>
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    placeholder={formDataErr.emailErr}
-                    value={formData.email}
-                    onChange={handleChangeInput}
-                    className="block w-full rounded-md border-0 py-1.5 text-center text-gray-950 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-700 sm:text-sm sm:leading-6 " 
-                  />
-                  {formDataErr.emailErr && <p className="text-red-600 text-xs mt-1">{formDataErr.emailErr}</p>}
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <div className="flex flex-row justify-between">
-                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                      Password
-                    </label>
-                    <PopoverInfo className="mx-4 p-3" btnContent="i" title="Password costraints"  description={<ul className="list-disc"><li>Minimum length of 8 characters</li><li>Must contain at least one uppercase letter.</li><li>Must contain at least one number.</li><li>Must contain at least one special character.</li></ul>} ></PopoverInfo>
-                  </div>
-                  
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-blue-950 hover:text-blue-700">
-                      Password dimenticata?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    value={formData.password}
-                    onChange={handleChangeInput}
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-950 text-center shadow-sm ring-1 ring-inset ring-gray-300  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-700 sm:text-sm sm:leading-6"
-                  />
-                  {formDataErr.passwordErr && <p className="text-red-600 text-xs mt-1">{formDataErr.passwordErr}</p>}
-                </div>
-              </div>
-  
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-blue-950 px-3 py-1.5 text-sm font-semibold leading-6 text-center text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign up
-                </button>
-              </div>
-              <div className="w-full flex items-center justify-center">
-                  <GoogleLogin
-                    onSuccess={(credentialResponse)=>handleGoogleLogin(credentialResponse)}
-                    onError={() => {
-                      console.log("Login Failed");
-                    }}
-                  />
-              </div>
-            </form>
-            <div>
-            
-            </div>
-  
-            
-          </div>
-        </div>
-      </>
-    )
+      })
+      .catch((e) => console.log(e));
   }
+
+  async function handleGoogleLogin(credentialResponse) {
+    try {
+      const id_token = credentialResponse.credential;
+      const bodyForm = {
+        token: id_token,
+      };
+
+      const response = await fetch(`${API_BASE_URL}users/auth/google`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bodyForm),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        login(data.token); // Assume server sends token as { token: 'jwt_token' }
+        navigate("/");
+      } else {
+        throw new Error("Google authentication failed");
+      }
+    } catch (error) {
+      console.error("Error:", error.message);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 pt-16"> {/* Added pt-16 */}
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img className="mx-auto h-28 w-auto" src={UniDomusLogo} alt="Unidomus" />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign up to a new account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit} method="POST">
+            <div>
+              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="username"
+                  required
+                  value={formData.username}
+                  onChange={handleChangeInput}
+                  className="block w-full rounded-md border-0 py-1.5 text-center text-gray-950 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-700 sm:text-sm sm:leading-6"
+                />
+                {formDataErr.usernameErr && <p className="text-red-600 text-xs mt-1">{formDataErr.usernameErr}</p>}
+              </div>
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  value={formData.email}
+                  onChange={handleChangeInput}
+                  className="block w-full rounded-md border-0 py-1.5 text-center text-gray-950 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-700 sm:text-sm sm:leading-6"
+                />
+                {formDataErr.emailErr && <p className="text-red-600 text-xs mt-1">{formDataErr.emailErr}</p>}
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <div className="flex flex-row justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Password
+                  </label>
+                  <PopoverInfo
+                    className="mx-4 p-3"
+                    btnContent="i"
+                    title="Password constraints"
+                    description={
+                      <ul className="list-disc">
+                        <li>Minimum length of 8 characters</li>
+                        <li>Must contain at least one uppercase letter.</li>
+                        <li>Must contain at least one number.</li>
+                        <li>Must contain at least one special character.</li>
+                      </ul>
+                    }
+                  ></PopoverInfo>
+                </div>
+
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-blue-950 hover:text-blue-700">
+                    Password dimenticata?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  value={formData.password}
+                  onChange={handleChangeInput}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-950 text-center shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-700 sm:text-sm sm:leading-6"
+                />
+                {formDataErr.passwordErr && <p className="text-red-600 text-xs mt-1">{formDataErr.passwordErr}</p>}
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-blue-950 px-3 py-1.5 text-sm font-semibold leading-6 text-center text-white shadow-sm hover:bg-blue-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign up
+              </button>
+            </div>
+            <div className="w-full flex items-center justify-center">
+              <GoogleLogin
+                onSuccess={handleGoogleLogin}
+                onError={() => {
+                  console.log("Login Failed");
+                }}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
